1302758f41ba1f0323f630230d6c2cd0e95fc6a8
Thambidurai-Mahendran <84059153+Thambidurai-Mahendran@users.noreply.github.com>
2021-05-12 11:36:14 -0400

    Update isValidHexCode.cpp

--- 12-05-2021/isValidHexCode.cpp
+++ 12-05-2021/isValidHexCode.cpp
@@ -1,14 +1,14 @@
 bool isValidHexCode(std::string str) {
 	  if(str[0]!='#'||str.size()!=7)
-		{
-			return false;
-		}
+	  {
+		return false;
+	  }
 	  for(int i=1;i<str.size();i++)
 	  {
-		    if(!((str[i]>='a'&&str[i]<='f')||(str[i]>='A'&&str[i]<='F')||(str[i]>='0'&&str[i]<='9'))) 
-			 {
-					 return false;	 
-			 }
-		}
-	 return true;
+		if(!((str[i]>='a'&&str[i]<='f')||(str[i]>='A'&&str[i]<='F')||(str[i]>='0'&&str[i]<='9'))) 
+	        {
+	         	 return false;	 
+	        }
+	  }
+	  return true;
 }
