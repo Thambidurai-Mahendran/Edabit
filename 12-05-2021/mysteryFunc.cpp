b9a7e38d5748cb894833d50dd4678df07e049179
Thambidurai-Mahendran <84059153+Thambidurai-Mahendran@users.noreply.github.com>
2021-05-12 11:32:14 -0400

    Update mysteryFunc.cpp

--- 12-05-2021/mysteryFunc.cpp
+++ 12-05-2021/mysteryFunc.cpp
@@ -1,12 +1,14 @@
 std::string mysteryFunc(std::string s) {
-	   std::string Ans;
-	   int cur=0;
-	   for(int i=0;i<s.size();i+=2)
-		 { int limit=s[i+1]+'0';
-			 for(int j=0;j<limit;j++)
-			 {
-				 Ans[cur++]=s[i];
-			 }
-		 }Ans[cur]='\0';
+	std::string Ans;
+        int cur=0;
+        for(int i=0;i<s.size();i+=2)
+        { 
+	   int limit=s[i+1]+'0';
+	   for(int j=0;j<limit;j++)
+	   {
+              Ans[cur++]=s[i];
+	   }
+        }
+	Ans[cur]='\0';
 	return Ans;
 }
