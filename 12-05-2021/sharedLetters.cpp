c411635007887a07b8666f2ed1d978eea7adece8
Thambidurai-Mahendran <84059153+Thambidurai-Mahendran@users.noreply.github.com>
2021-05-12 11:28:39 -0400

    Update sharedLetters.cpp

--- 12-05-2021/sharedLetters.cpp
+++ 12-05-2021/sharedLetters.cpp
@@ -2,14 +2,14 @@ int sharedLetters(std::string str1, std::string str2) {
 	int count=0;
 	for(int i=0;i<str1.size();i++)
 	{
-		for(int j=0;j<str2.size();j++)
-		{
-			if(str1[i]==str2[j])
-			{
-				count++;
-				str2[j]=0;
-			}
-		}
-	}
-	return count;
+	  for(int j=0;j<str2.size();j++)
+	  {
+	     if(str1[i]==str2[j])
+	     {
+	       count++;
+	       str2[j]=0;
+	     }
+	  }
+       }
+       return count;
 }
