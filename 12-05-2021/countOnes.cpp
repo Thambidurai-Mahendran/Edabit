355c175a144e1e71cc8a846788297db471123534
Thambidurai-Mahendran <84059153+Thambidurai-Mahendran@users.noreply.github.com>
2021-05-12 11:25:12 -0400

    Create countOnes.cpp

---
+++ 12-05-2021/countOnes.cpp
@@ -0,0 +1,18 @@
+int countOnes(std::vector<int> arr) {
+	 int countOfOnes=0;
+	 for(int i=0;i<arr.size();)
+	 {  
+		  if(arr[i]==1&&arr[i+1]==1)
+			{  countOfOnes++;
+				do
+		    {
+			    i++;
+		    }while(arr[i]!=0);
+			}
+		  else
+			{
+				i++;
+			} 
+	 }
+	return  countOfOnes;
+}
