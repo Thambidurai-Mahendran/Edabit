8a70aeb13a3ca7f726c5793c7c36e1553882a0db
Thambidurai-Mahendran <84059153+Thambidurai-Mahendran@users.noreply.github.com>
2021-05-12 05:24:02 -0400

    Update makeBox.cpp

--- 12-05-2021/makeBox.cpp
+++ 12-05-2021/makeBox.cpp
@@ -2,17 +2,17 @@ std::vector<std::string> makeBox(int n) {
 	std::vector<std::string>Box;
 	 for(int i=0;i<n;i++)
 	 {   
-		  for(int j=0;j<n;j++)
-		  {
-			    if(i==0||i==n-1||j==0||j==n-1)
-				  {
-					  Box[i][j]='#';
-				  }
-				  else 
-					{
-						Box[i][j]=' ';
-					}
-		  }
+	   for(int j=0;j<n;j++)
+	   {
+	     if(i==0||i==n-1||j==0||j==n-1)
+	     {
+		  Box[i][j]='#';
+	     }
+	     else 
+	     {
+		  Box[i][j]=' ';
+	     }
+	   }
 	 }
 	return Box;
 }
