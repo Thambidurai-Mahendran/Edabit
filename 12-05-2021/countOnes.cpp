622793f6dc006198b99db5e3dfc5fdb3ec73e129
Thambidurai-Mahendran <84059153+Thambidurai-Mahendran@users.noreply.github.com>
2021-05-12 11:38:10 -0400

    Update countOnes.cpp

--- 12-05-2021/countOnes.cpp
+++ 12-05-2021/countOnes.cpp
@@ -2,17 +2,18 @@ int countOnes(std::vector<int> arr) {
 	 int countOfOnes=0;
 	 for(int i=0;i<arr.size();)
 	 {  
-		  if(arr[i]==1&&arr[i+1]==1)
-			{  countOfOnes++;
-				do
-		    {
-			    i++;
-		    }while(arr[i]!=0);
-			}
-		  else
-			{
-				i++;
-			} 
+	    if(arr[i]==1&&arr[i+1]==1)
+	    {  
+		countOfOnes++;
+		do
+	        {
+		  i++;
+		}while(arr[i]!=0);
+	    }
+	    else
+            {
+		i++;
+	    } 
 	 }
 	return  countOfOnes;
 }
