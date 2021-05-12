4bd9d445072ec5dea5ca00366beff671b8a3e53b
Thambidurai-Mahendran <84059153+Thambidurai-Mahendran@users.noreply.github.com>
2021-05-12 11:09:55 -0400

    Create isValidHexCode.cpp

---
+++ 12-05-2021/isValidHexCode.cpp
@@ -0,0 +1,14 @@
+bool isValidHexCode(std::string str) {
+	  if(str[0]!='#'||str.size()!=7)
+		{
+			return false;
+		}
+	  for(int i=1;i<str.size();i++)
+	  {
+		    if(!((str[i]>='a'&&str[i]<='f')||(str[i]>='A'&&str[i]<='F')||(str[i]>='0'&&str[i]<='9'))) 
+			 {
+					 return false;	 
+			 }
+		}
+	 return true;
+}
