bfb83f3991a306bea1235979d79da930767d172e
Thambidurai-Mahendran <84059153+Thambidurai-Mahendran@users.noreply.github.com>
2021-05-12 11:40:26 -0400

    Update cons.cpp

--- 12-05-2021/cons.cpp
+++ 12-05-2021/cons.cpp
@@ -1,26 +1,27 @@
 bool cons(std::vector<int> arr) {
 	   int min=arr[0];
 	   for(int i=0;i<arr.size();i++)
-		 {
-			   if(min>arr[i])
-			   {
-				     min=arr[i];
-			   }
-		 }
+	   {
+               if(min>arr[i])
+	       {
+		     min=arr[i];
+	       }
+	   }
 	   for(int i=1;i<arr.size();i++)
-		 {   min++;
-			  int flag=0;
-			  for(int j=0;j<arr.size();j++)
-				{
-					  if(arr[j]==min)
-						{
-							flag=1;
-						}
-				}
-			  if(flag==0)
-			  {
-				  return false;
-			  }
-		 }
-	   return true;
+	   {   
+		   min++;
+		   int flag=0;
+		   for(int j=0;j<arr.size();j++)
+		   {
+			 if(arr[j]==min)
+		         {
+				flag=1;
+			 }
+		   }
+		   if(flag==0)
+		   {
+		      return false;
+	           }
+	 }
+	 return true;
 }
