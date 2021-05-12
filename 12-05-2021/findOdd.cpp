4fa7bdc86a0e1c33255af78bdfc5018a4c17de0c
Thambidurai-Mahendran <84059153+Thambidurai-Mahendran@users.noreply.github.com>
2021-05-12 04:16:37 -0400

    Create findOdd.cpp

---
+++ 12-05-2021/findOdd.cpp
@@ -0,0 +1,18 @@
+using namespace std;
+
+int findOdd(vector<int> arr) {
+   	for(int i=0;i<arr.size();i++)
+	  {   int count=0;
+		    for(int j=0;j<arr.size();j++)
+				{
+					  if(arr[i]==arr[j])
+					  {
+						    count++;
+					  }
+				}
+		    if(count%2==1)
+				{
+					return arr[i];
+				}
+	  }
+}
