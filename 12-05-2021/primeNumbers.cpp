90bfe980c994be27f27c990cadc8f8c7f3cc7cac
Thambidurai-Mahendran <84059153+Thambidurai-Mahendran@users.noreply.github.com>
2021-05-12 11:30:26 -0400

    Update primeNumbers.cpp

--- 12-05-2021/primeNumbers.cpp
+++ 12-05-2021/primeNumbers.cpp
@@ -1,18 +1,19 @@
 int primeNumbers(int num) {
-	  int primeCounts=0;
-	  for(int i=2;i<=num;i++)
-		{  int count=0;
-			 for(int j=2;j<i;j++)
-			 {
-				 if(i%j==0)
-				 {
-					 count++;
-				 }
-			 }
-		   if(count==0)
-			 {
-			  	primeCounts++;
-			 }
-		}
-	 return primeCounts;
+ int primeCounts=0;
+ for(int i=2;i<=num;i++)
+ {
+    int count=0;
+    for(int j=2;j<i;j++)
+    {
+      if(i%j==0)
+      {
+	 count++;
+      }
+    }
+    if(count==0)
+    {
+	primeCounts++;
+    }
+ }
+return primeCounts;
 }
