3d51f10e3abbc918020132aa29b08af1ae9a8787
Thambidurai-Mahendran <84059153+Thambidurai-Mahendran@users.noreply.github.com>
2021-05-13 12:29:40 -0400

    Update firstNonRepeatedCharacter.cpp

--- 13-05-2021/firstNonRepeatedCharacter.cpp
+++ 13-05-2021/firstNonRepeatedCharacter.cpp
@@ -7,16 +7,16 @@ std::string firstNonRepeatedCharacter(std::string str) {
 	for(int i=0;i<str.size();i++)
 	{   
 	    /*It is used to count the repeated characters count*/ 
-		repeatedCounts[str[i]]++;
+	    repeatedCounts[str[i]]++;
 	}
 	for(int i=0;i<str.size();i++)
 	{    
 	    /*If the character count is equal to 1 .it is a non repeating character .So, We use the If statement to find the non repeating character */ 
-		if(repeatedCounts[str[i]]==1)
-		{
-			nonRepeatedCharacter+=str[i];
-			return nonRepeatedCharacter;
-		}
+	    if(repeatedCounts[str[i]]==1)
+	    {
+	        nonRepeatedCharacter+=str[i];
+	        return nonRepeatedCharacter;
+	    }
 	}
 	/*If all characters are repeated then, we store the word "Invalid" in the string */
 	nonRepeatedCharacter+="Invalid";
