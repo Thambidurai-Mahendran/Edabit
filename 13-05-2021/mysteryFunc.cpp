28bf3cfde5091be778f32d5013945eefa617970d
Thambidurai-Mahendran <84059153+Thambidurai-Mahendran@users.noreply.github.com>
2021-05-13 11:21:02 -0400

    Update mysteryFunc.cpp

--- 13-05-2021/mysteryFunc.cpp
+++ 13-05-2021/mysteryFunc.cpp
@@ -7,18 +7,18 @@ std::string mysteryFunc(int n) {
 	 for(int i=0;i<mystryFun.size();)
 	 {   
 	     /*we store the character in c variable*/
-		 char c=mystryFun[i];
-		 int count=0;
-		 /*This dowhile loop used to calculate the count of continuous characters in a string*/
-		 do
-		 {
-			 i++;
-			 count++;
-		 }while(mystryFun[i]==c);
-		 /* When we add a integer variable 'count' with the character "0",count is converted from integer to character.then we store the count in the mystrChallenge string*/
-		mystryChallenge+=(char)count+'0';
-		/*we store the c variable in mystryChallenge string*/
-		mystryChallenge+=c;
+	     char c=mystryFun[i];
+	     int count=0;
+	     /*This dowhile loop used to calculate the count of continuous characters in a string*/
+	     do
+	     {
+	        i++;
+	        count++;
+	     }while(mystryFun[i]==c);
+	     /* When we add a integer variable 'count' with the character "0",count is converted from integer to character.then we store the count in the mystrChallenge string*/
+	     mystryChallenge+=(char)count+'0';
+	     /*we store the c variable in mystryChallenge string*/
+	     mystryChallenge+=c;
 	 }
 	/*Finally we return the mystryChallenge String */ 
    	return mystryChallenge;
