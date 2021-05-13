32ea10bc34726f7c83fb47322f9060d4d370ce75
Thambidurai-Mahendran <84059153+Thambidurai-Mahendran@users.noreply.github.com>
2021-05-13 10:30:32 -0400

    Update battleOutcome.cpp

--- 13-05-2021/battleOutcome.cpp
+++ 13-05-2021/battleOutcome.cpp
@@ -1,12 +1,12 @@
 int battleOutcome(int num) {
-	 /*First we convert the num integer to string ,Its very useful to iterate index vise*/
+	 /*First we convert the num integer to string ,Its very useful to iterate index wise*/
 	 std::string numStr=std::to_string(num);
 	 /*create battleOut string */ 
 	 std::string battleOut;
 	 /*iterate the full string and the index is incremented by two each steps*/
 	 for(int i=0;i<numStr.size();i+=2)
 	 {  
-	        /*if numStr[i] andnumStr[i+1]  values are different we find the big value and add it to the battleOutstring*/
+	        /*if numStr[i] and numStr[i+1]  values are different we find the big value and add it to the battleOut string*/
 		if(numStr[i]!=numStr[i+1])
 		{
 		   battleOut+=(numStr[i]>numStr[i+1])?numStr[i]:numStr[i+1];
