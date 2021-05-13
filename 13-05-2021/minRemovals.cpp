d526b810b33eeb066231ab254b07fe56593f08a8
Thambidurai-Mahendran <84059153+Thambidurai-Mahendran@users.noreply.github.com>
2021-05-13 05:02:22 -0400

    Update minRemovals.cpp

--- 13-05-2021/minRemovals.cpp
+++ 13-05-2021/minRemovals.cpp
@@ -4,17 +4,18 @@ int minRemovals(std::string str1, std::string str2) {
 	/*Iterate the first string*/
 	for(int i=0;i<str1.size();i++)
 	{  
-     /*Iterate the second string */
+           /*Iterate the second string */
 	   for(int j=0;j<str2.size();j++)
-	   {    /*we use the if statement to check the same letters in a string */
+	   {   
+		/*we use the if statement to check the same letters in a string */
 	        if(str1[i]==str2[j])
-			    {
-			      count++;
-			      str2[j]=0;
-			      break;
-			    }
-		 }
-  }
+	        {
+	           count++;
+	           str2[j]=0;
+	           break;
+	        }
+	   }
+        }
 	/*If we get the same letters count then we subtract same letters count in each strings and we get the Minimum Removals to Make Two Strings Anagrams */ 
 	return str1.size()-count+str2.size()-count;
 }
