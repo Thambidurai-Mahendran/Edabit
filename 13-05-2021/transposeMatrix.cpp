751d7d250132979514944a0dad4ecee3ce3418b0
Thambidurai-Mahendran <84059153+Thambidurai-Mahendran@users.noreply.github.com>
2021-05-13 10:24:44 -0400

    Update transposeMatrix.cpp

--- 13-05-2021/transposeMatrix.cpp
+++ 13-05-2021/transposeMatrix.cpp
@@ -1,18 +1,18 @@
 using namespace std; 
 
 vector<vector<int>> transposeMatrix(vector<vector<int>> arr) {
-	    /*create Two dimensional integer array*/
-	    vector<vector<int>> matrixTranspose;
-	    /*iterate row wise*/
-	    for(int i=0;i<arr.size();i++)
-		{   
-		    /*Iterate column wise*/
-			for(int j=0;j<arr[i].size();j++)
-			{
-				/*Interchange the row and matrix to get transpose matrixc*/
-				matrixTranspose[j][i]=arr[i][j];
-			}
-		}
-		/*Finally we return the transpose matrix*/
-	    return matrixTranspose;
+     /*create Two dimensional integer array*/
+     vector<vector<int>> matrixTranspose;
+     /*iterate row wise*/
+     for(int i=0;i<arr.size();i++)
+     {   
+	 /*Iterate column wise*/
+	 for(int j=0;j<arr[i].size();j++)
+	 {
+	     /*Interchange the row and matrix to get transpose matrixc*/
+	     matrixTranspose[j][i]=arr[i][j];
+	 }
+     }
+     /*Finally we return the transpose matrix*/
+     return matrixTranspose;
 }
