201df92d2f6c2a67951d720002872aaf88ed2efd
Thambidurai-Mahendran <84059153+Thambidurai-Mahendran@users.noreply.github.com>
2021-05-13 04:17:41 -0400

    Update initialize.cpp

--- 13-05-2021/initialize.cpp
+++ 13-05-2021/initialize.cpp
@@ -6,22 +6,22 @@ vector<string> initialize(vector<string> names) {
 	{    
 	    /*We must store the First letter of the string */
 	    /*We use push_back() function to store the character in a string*/
-		  initials[i].push_back(names[i][0]);
-		  /*We store the '.' character into the String*/
+	    initials[i].push_back(names[i][0]);
+	    /*We store the '.' character into the String*/
 	    initials[i].push_back('.');
 	    /*Iterate the full string*/
-		  for(int j=0;j<names.size();j++)
-		  {   
+	    for(int j=0;j<names.size();j++)
+	    {   
 		     /*If the string contain two words its separated by the space So,We must include space and Next letter after the space and '.' character*/
-			   if(names[i][j]==' ')
-			   {
-			    	initials[i].push_back(' ');
-				    initials[i].push_back(names[i][j+1]);
-				    initials[i].push_back('.');
-				    break;
-			   }
+		     if(names[i][j]==' ')
+	             {
+			   initials[i].push_back(' ');
+		           initials[i].push_back(names[i][j+1]);
+		           initials[i].push_back('.');
+			   break;
+		     }
 				
-		  }
+	    }
 	}
 	/*Finally We return the array of initials*/
 	return initials;
