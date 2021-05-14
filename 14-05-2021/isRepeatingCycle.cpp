bdcd14770cb6e33a2bc977b6fdb3690a8baea0f3
Thambidurai-Mahendran <84059153+Thambidurai-Mahendran@users.noreply.github.com>
2021-05-14 10:19:21 -0400

    Update isRepeatingCycle.cpp

--- 14-05-2021/isRepeatingCycle.cpp
+++ 14-05-2021/isRepeatingCycle.cpp
@@ -1,8 +1,8 @@
 bool isRepeatingCycle(std::vector<int> arr, int length) {
 	/*If the array size is less than or equal to length ,we return true*/
 	if(length>=arr.size())
-		return true;
-    /* below looping is used to find its repeating cycle or not .If it is a non repeating cycle we return false */		
+	   return true;
+        /* below looping is used to find its repeating cycle or not .If it is a non repeating cycle we return false */		
 	for(int i=0;i<arr.size()-length;i++)
 	{
 		if(arr[i]!=arr[i+length])
