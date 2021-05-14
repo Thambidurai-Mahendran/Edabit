845d345eebe0e41c0b8389bb8aebbdf2e1caf724
Thambidurai-Mahendran <84059153+Thambidurai-Mahendran@users.noreply.github.com>
2021-05-14 10:35:17 -0400

    Update xPronounce.cpp

--- 14-05-2021/xPronounce.cpp
+++ 14-05-2021/xPronounce.cpp
@@ -3,8 +3,8 @@ std::string xPronounce(std::string sentence) {
 	string pronounceTheXs;
 	/*Replace all x's with "cks" UNLESS:
 
-    The word begins with "x", therefore replace it with "z".
-    The word is just the letter "x", therefore replace it with "ecks". */
+        The word begins with "x", therefore replace it with "z".
+        The word is just the letter "x", therefore replace it with "ecks". */
 	for(int i=0;i<sentence.size();i++)
 	{
 		if(sentence[i]=='x'&&sentence[i-1]==' '&&sentence[i+1]!=' ')
@@ -13,7 +13,7 @@ std::string xPronounce(std::string sentence) {
 		}
 		else if(sentence[i]=='x'&&sentence[i-1]==' '&&sentence[i+1]==' ')
 		{
-					pronounceTheXs+="ecks";
+			pronounceTheXs+="ecks";
 		}
 		else if(sentence[i]=='x')
 		{
